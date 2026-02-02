ca72c20c36478a090f283d2692c7972ef796b443
iury <joseiury74@gmail.com>
2026-02-02 14:33:52 -0300

    Atividade 2 Questão 10

---
+++ src/Atividade_2/Q10/Main.java
@@ -0,0 +1,34 @@
+package Atividade_2.Q10;
+
+import java.util.ArrayList;
+
+class Pagamento{
+    public void procesarPagamento(double valor){
+        System.out.println("Processando pagamento genérico de R$ "+valor);
+    }
+}
+
+class PagamentoCartao extends Pagamento{
+    @Override
+    public void procesarPagamento(double valor) {
+        System.out.println("Processando pagamento com cartão de crédito: R$ "+valor);
+    }
+}
+
+class PagamentoPix extends Pagamento{
+    @Override
+    public void procesarPagamento(double valor) {
+        System.out.println("Processando pagamento via PIX: R$ "+valor);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Pagamento> pagamentos = new ArrayList<>();
+        pagamentos.add(new PagamentoCartao());
+        pagamentos.add(new PagamentoPix());
+
+        for(Pagamento pagamento:pagamentos){
+            pagamento.procesarPagamento(100);
+        }
+    }
+}
