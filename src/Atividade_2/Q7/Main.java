e172b5805502e557287fca33bfe96d59bc447f8e
iury <joseiury74@gmail.com>
2026-02-02 13:50:04 -0300

    Atividade 2 Questão 7

---
+++ src/Atividade_2/Q7/Main.java
@@ -0,0 +1,31 @@
+package Atividade_2.Q7;
+
+class Animal{
+    public void fazerSom(){
+        System.out.println("...");
+    }
+}
+
+class Cachorro extends Animal{
+    @Override
+    public void fazerSom(){
+        System.out.println("Auau");
+    }
+}
+
+class Gato extends Animal{
+    @Override
+    public void fazerSom(){
+        System.out.println("Miau");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animais = new Animal[2];
+        animais[0] = new Cachorro();
+        animais[1] = new Gato();
+
+        animais[0].fazerSom();
+        animais[1].fazerSom();
+    }
+}
