6ef331f34e8ba4721a206c03e7ad12acbc3c4e40
iury <joseiury74@gmail.com>
2026-02-02 13:57:07 -0300

    Atividade 2 Questão 6

---
+++ src/Atividade_2/Q6/Main.java
@@ -0,0 +1,44 @@
+package Atividade_2.Q6;
+
+enum Departamento{
+    RH,ADMINISTRATIVO,GERENCIA
+}
+
+class Funcionario{
+    private String nome;
+    private double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
+class Gerente extends Funcionario{
+    public Gerente(Departamento departamento, String nome, double salario) {
+        super(nome, salario);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente(Departamento.GERENCIA, "Lucas", 4000.00);
+    }
+
+}
